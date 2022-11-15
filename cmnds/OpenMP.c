c4dca37a174eed287152d0433da69bef857e1290
Nivedha027 <89121294+Nivedha027@users.noreply.github.com>
2022-11-15 19:52:29 +0530

    Add files via upload

---
+++ cmnds/OpenMP.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>  
+#define N 3
+
+int A[N][N];
+int B[N][N];
+int C[N][N];
+
+int main()
+{
+    int i,j,k;
+
+    omp_set_num_threads(omp_get_num_procs());
+    for (i= 0; i< N; i++)
+        for (j= 0; j< N; j++)
+        {
+            A[i][j] = 2;
+            B[i][j] = 5;
+        }
+
+    #pragma omp parallel for private(i,j,k) shared(A,B,C)
+
+    for (i = 0; i < N; ++i)
+    {
+        for (j = 0; j < N; ++j)
+        {
+            for (k = 0; k < N; ++k)
+                {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+        }
+    }
+
+    for (i = 0; i < N; ++i)
+    {
+        for (j = 0; j < N; ++j)
+        {
+            printf("%d ",C[i][j]) ;         
+        }
+        printf("\n");
+    }
+}
